91190a65940868d6f85073510d8bbe168d7b25a6
MohammadGudy <muhammadgoudarzi@outlook.com>
2025-10-20 14:45:56 -0400

    Colliders bug fixed

--- Royal Run/Assets/Scripts/Chunk.cs
+++ Royal Run/Assets/Scripts/Chunk.cs
@@ -1,9 +1,12 @@
+using System.Collections.Generic;
+using NUnit.Framework;
 using UnityEngine;
 
 public class Chunk : MonoBehaviour
 {
 
     [SerializeField] GameObject fencePrefab;
+    [SerializeField] float[] lanes = { -4.19f, -1.94f, 0.72f };
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -15,7 +18,20 @@ public class Chunk : MonoBehaviour
 
     void SpawnFence()
     {
-        //Vector3 spawnPosition = new Vector3();
+        List<int> availableLanes = new List<int> { 0, 1, 2 };
+        int fencesToSpawn = Random.Range(0, 3);
+
+
+        for (int i = 0; i < fencesToSpawn; i++)
+        {
+            int randomLaneIndex = Random.Range(0, availableLanes.Count);
+            int selectedLane = availableLanes[randomLaneIndex];
+            availableLanes.RemoveAt(randomLaneIndex);
+
+            Vector3 spawnPosition = new Vector3(lanes[selectedLane], transform.position.y, transform.position.z);
+            Instantiate(fencePrefab, spawnPosition, Quaternion.identity, this.transform);
+        }
+
     }
 
     // Update is called once per frame
