f815f2c6de633611b784a0fa0f463cc3fe7e6e1d
MohammadGudy <muhammadgoudarzi@outlook.com>
2025-10-21 12:16:29 -0400

    added first version of Pickup.cs

--- Royal Run/Assets/Scripts/Chunk.cs
+++ Royal Run/Assets/Scripts/Chunk.cs
@@ -6,19 +6,27 @@ public class Chunk : MonoBehaviour
 {
 
     [SerializeField] GameObject fencePrefab;
+    [SerializeField] GameObject applePrefab;
+    [SerializeField] GameObject coinPrefab;
+    [SerializeField] float appleSpawnChance = 0.3f;
+    [SerializeField] float coinSpawnChance = 0.5f;
+    [SerializeField] float coinSeperationLength = 2f;
+
     [SerializeField] float[] lanes = { -4.19f, -1.94f, 0.72f };
+     List<int> availableLanes = new List<int> { 0, 1, 2 };
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        SpawnFence();
+        SpawnFences();
+        SpawnApple();
+        SpawnCoin();
 
     }
 
-    void SpawnFence()
+    void SpawnFences()
     {
-        List<int> availableLanes = new List<int> { 0, 1, 2 };
         int fencesToSpawn = Random.Range(0, lanes.Length);
 
 
@@ -26,12 +34,42 @@ public class Chunk : MonoBehaviour
         {
             if (availableLanes.Count <= 0) break;
 
-            int randomLaneIndex = Random.Range(0, availableLanes.Count);
-            int selectedLane = availableLanes[randomLaneIndex];
-            availableLanes.RemoveAt(randomLaneIndex);
+            int selectedLane = SelectLane();
 
             Vector3 spawnPosition = new Vector3(lanes[selectedLane], transform.position.y, transform.position.z);
             Instantiate(fencePrefab, spawnPosition, Quaternion.identity, this.transform);
+        }
+
+    }
+
+    void SpawnApple()
+    {
+        if (Random.value > appleSpawnChance || availableLanes.Count <= 0) return;
+
+        int selectedLane = SelectLane();
+
+        Vector3 spawnPosition = new Vector3(lanes[selectedLane], transform.position.y, transform.position.z);
+        Instantiate(applePrefab, spawnPosition, Quaternion.identity, this.transform);
+    }
+
+    void SpawnCoin()
+    {
+        if (Random.value > coinSpawnChance || availableLanes.Count <= 0) return;
+
+        int selectedLane = SelectLane();
+
+        int maxCoinsToSpawn = 6;
+        int coinsToSpawn = Random.Range(1, maxCoinsToSpawn);
+        float topOfChunkZPos = transform.position.z + (coinSeperationLength * 2f);
+        
+        for (int i = 0; i < coinsToSpawn; i++)
+
+        {
+
+        float spawnPositionZ = topOfChunkZPos - (i * coinSeperationLength);
+        Vector3 spawnPosition = new Vector3(lanes[selectedLane], transform.position.y, spawnPositionZ);
+        Instantiate(coinPrefab, spawnPosition, Quaternion.identity, this.transform);
+            
         }
 
     }
@@ -40,5 +78,13 @@ public class Chunk : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private int SelectLane()
+    {
+        int randomLaneIndex = Random.Range(0, availableLanes.Count);
+        int selectedLane = availableLanes[randomLaneIndex];
+        availableLanes.RemoveAt(randomLaneIndex);
+        return selectedLane;
     }
 }
