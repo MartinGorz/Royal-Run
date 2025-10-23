e96d7f6747bb56a6828cb35501da4e19d10b5219
MohammadGudy <muhammadgoudarzi@outlook.com>
2025-10-23 01:37:19 -0400

    Added ScoreManager.cs

--- Royal Run/Assets/Scripts/Pickups/Coin.cs
+++ Royal Run/Assets/Scripts/Pickups/Coin.cs
@@ -2,8 +2,15 @@ using UnityEngine;
 
 public class Coin : Pickup
 {
+    [SerializeField] int scoreAmount = 100;
+    ScoreManager scoreManager;
+
+    private void Start()
+    {
+        scoreManager = FindFirstObjectByType<ScoreManager>();
+    }
     protected override void OnPickup()
     {
-        Debug.Log("Add 100 points.");
+        scoreManager.IncreaseScore(100);
     }
 }
